037a99f89b7c5dda48666d38a2773e8312e251f3
felipe <felipemoreno148@gmail.com>
2024-09-30 19:31:15 -0300

    teste getStaticProps()

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ function Home() {
     return  <div>
         <h1>Home</h1>
         <Link href="/sobre">Acesse o sobre</Link>
+        <p>
+            <Link href="/tempo">Veja data/hora</Link>
+        </p>
     </div>
 }
 
---
+++ pages/tempo.js
@@ -0,0 +1,25 @@
+import Link from "next/link"
+
+function tempo(props) {
+    const dynamicDate = new Date();
+    const dynamicDateString = dynamicDate.toGMTString();
+    return (
+        <div>
+            {dynamicDateString} (dinâmico)
+            <p>{props.staticDateString} (estático)</p>
+            <p><Link href="/">Voltar a Homepage</Link></p>
+        </div>
+    )
+}
+
+export function getStaticProps() {
+    const staticDate = new Date();
+    const staticDateString = staticDate.toGMTString();
+    return {
+        props: {
+            staticDateString
+        }
+    }
+};
+
+export default tempo;
