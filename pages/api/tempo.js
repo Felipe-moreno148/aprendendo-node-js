7f307b8afb5c862c94d3095842151db3000e215d
felipe <felipemoreno148@gmail.com>
2024-10-05 15:11:19 -0300

    esconde info sensivel

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,18 @@
-function exemplo(request, response) {
+async function exemplo(request, response) {
+    const segredo = process.env.SEGREDO;
     const dynamicDate = new Date();
+
+    const exemplo1 = await fetch(`informação sensível${segredo}`)
+    const exemplo1Json = await exemplo1.json();
+    const exemplo2 = exemplo1Json.info;
 
     response.json({
         date: dynamicDate.toGMTString()
-    });
+    },
+    {
+        ex2: exemplo2.toGMTString()
+    },
+);
 }
 
 export default exemplo;
